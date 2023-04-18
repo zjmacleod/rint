173092b319714e56c0ccaef98e1fb990d11fa429
Zander Macleod <zmacleod@pm.me>
2023-04-18 13:58:48 +0200

    Add comment

--- src/integration/basic.rs
+++ src/integration/basic.rs
@@ -87,6 +87,7 @@ where
         };
         let initial_abs = initial_kronrod.abs();
 
+        // XXX Can we get rid of this additional allocation?
         let mut function_values: Vec<(f64, (f64, f64))> = Vec::with_capacity(61);
 
         let (gauss_result, kronrod_shared, abs_shared) = self
