570b3e08a14f96bb2f774b2bdac5a236e44ef6a7
Zander Macleod <zmacleod@pm.me>
2023-04-27 20:55:59 +0200

    Import tests of Adaptive from GSL

--- tests/adaptive-smooth-positive-function.rs
+++ tests/adaptive-smooth-positive-function.rs
@@ -1,5 +1,9 @@
+use gauss_kronrod_integration::integration::adaptive::Kind;
 use gauss_kronrod_integration::integration::{ErrorBound, GaussKronrodAdaptive};
-use gauss_kronrod_integration::rule::{GaussKronrod15, GaussKronrod21};
+use gauss_kronrod_integration::rule::{
+    GaussKronrod15, GaussKronrod21, GaussKronrod31, GaussKronrod51,
+    GaussKronrod61,
+};
 
 mod util;
 
@@ -188,6 +192,307 @@ fn test_adaptive_smooth_absolute_error_21() -> Result<(), String> {
         exp_iterations,
         "adaptive qk15(f1) smooth iterations",
     )?;
+
+    Ok(())
+}
+
+// Test oscillating Function3 with 31 point adaptive integration and absolute error
+// bound. Should terminate due to roundoff error.
+#[test]
+fn test_adaptive_oscillating_should_error_roundoff_31() -> Result<(), String> {
+    let exp_result = -7.238969575482959717E-01;
+    let exp_abserr = 1.285805464427459261E-14;
+    let exp_iterations = 1;
+
+    let error_bound = ErrorBound::Absolute(1e-14);
+
+    let abs_error_bound = 1e-15;
+    let rel_error_bound = 1e-6;
+    let rule = GaussKronrod31;
+    let alpha = 1.3;
+
+    let lower = 0.3;
+    let upper = 2.71;
+
+    let function = util::Function3 { alpha };
+
+    let integral = GaussKronrodAdaptive::new(
+        lower,
+        upper,
+        error_bound,
+        rule,
+        &function,
+        1000,
+    )
+    .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::FailedToReachToleranceRoundoff = err.kind() {
+            let result = err.result();
+            let error = err.error();
+            let iterations = err.iterations();
+
+            util::test_relative_error(
+                result,
+                exp_result,
+                abs_error_bound,
+                "qk15(f1) smooth result",
+            )?;
+            util::test_relative_error(
+                error,
+                exp_abserr,
+                rel_error_bound,
+                "qk15(f1) smooth abserr",
+            )?;
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive qk15(f1) smooth iterations",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
+
+    let error_bound = ErrorBound::Absolute(1e-14);
+    let lower = 2.71;
+    let upper = 0.3;
+
+    let function = util::Function3 { alpha };
+
+    let integral = GaussKronrodAdaptive::new(
+        lower,
+        upper,
+        error_bound,
+        rule,
+        &function,
+        1000,
+    )
+    .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::FailedToReachToleranceRoundoff = err.kind() {
+            let result = err.result();
+            let error = err.error();
+            let iterations = err.iterations();
+
+            util::test_relative_error(
+                result,
+                -exp_result,
+                abs_error_bound,
+                "qk15(f1) smooth result",
+            )?;
+            util::test_relative_error(
+                error,
+                exp_abserr,
+                rel_error_bound,
+                "qk15(f1) smooth abserr",
+            )?;
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive qk15(f1) smooth iterations",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
+
+    Ok(())
+}
+
+// Test Function16 with 51 point adaptive integration and absolute error
+// bound. Should terminate due to singularity detected (singularity at x=-0.1).
+#[test]
+fn test_adaptive_singularity_51() -> Result<(), String> {
+    let exp_iterations = 51;
+
+    let error_bound = ErrorBound::Absolute(1e-14);
+
+    let rule = GaussKronrod51;
+    let alpha = 2;
+
+    let lower = -1.0;
+    let upper = 1.0;
+
+    let function = util::Function16 { alpha };
+
+    let integral = GaussKronrodAdaptive::new(
+        lower,
+        upper,
+        error_bound,
+        rule,
+        &function,
+        1000,
+    )
+    .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::PossibleSingularity { lower, upper } = err.kind() {
+            let iterations = err.iterations();
+
+            assert!(*lower < -0.1f64);
+            assert!(*upper > -0.1f64);
+
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive qk15(f1) smooth iterations",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
+
+    let error_bound = ErrorBound::Absolute(1e-14);
+    let lower = 1.0;
+    let upper = -1.0;
+
+    let function = util::Function16 { alpha };
+
+    let integral = GaussKronrodAdaptive::new(
+        lower,
+        upper,
+        error_bound,
+        rule,
+        &function,
+        1000,
+    )
+    .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::PossibleSingularity { lower, upper } = err.kind() {
+            let iterations = err.iterations();
+
+            assert!(*lower > -0.1f64);
+            assert!(*upper < -0.1f64);
+
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive qk15(f1) smooth iterations",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
+    Ok(())
+}
+
+// TODO: This should return an error, but we successfully integrate,
+// Test Function16 with 61 point adaptive integration and absolute error
+// bound. Should terminate due to max iterations reached
+#[test]
+fn test_adaptive_max_iterations_61() -> Result<(), String> {
+    let exp_result = 9.565151449233894709;
+    let exp_abserr = 1.570369823891028460E+01;
+    let exp_iterations = 3;
+
+    let abs_error_bound = 1e-15;
+    let rel_error_bound = 1e-6;
+    let error_bound = ErrorBound::Absolute(1e-14);
+
+    let rule = GaussKronrod61;
+    let alpha = 1;
+
+    let lower = -1.0;
+    let upper = 1.0;
+
+    let function = util::Function16 { alpha };
+
+    let integral =
+        GaussKronrodAdaptive::new(lower, upper, error_bound, rule, &function, 3)
+            .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::MaximumIterationsReached = err.kind() {
+            let result = err.result();
+            let error = err.error();
+            let iterations = err.iterations();
+
+            util::test_relative_error(
+                result,
+                exp_result,
+                abs_error_bound,
+                "adaptive(f16, 61) smooth result",
+            )?;
+            util::test_relative_error(
+                error,
+                exp_abserr,
+                rel_error_bound,
+                "adaptive(f16, 61) smooth abserr",
+            )?;
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive(f16, 61) smooth iterations",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
+
+    let error_bound = ErrorBound::Absolute(1e-14);
+    let lower = 1.0;
+    let upper = -1.0;
+
+    let function = util::Function16 { alpha };
+
+    let integral =
+        GaussKronrodAdaptive::new(lower, upper, error_bound, rule, &function, 3)
+            .unwrap();
+
+    let integral_result = integral.integrate();
+
+    if let Err(err) = integral_result {
+        if let Kind::MaximumIterationsReached = err.kind() {
+            let result = err.result();
+            let error = err.error();
+            let iterations = err.iterations();
+
+            util::test_relative_error(
+                result,
+                -exp_result,
+                abs_error_bound,
+                "adaptive(f16, 61) smooth result negative",
+            )?;
+            util::test_relative_error(
+                error,
+                exp_abserr,
+                rel_error_bound,
+                "adaptive(f16, 61) smooth abserr negative",
+            )?;
+            util::test_int(
+                iterations,
+                exp_iterations,
+                "adaptive(f16, 61) smooth iterations negative",
+            )?;
+        } else {
+            return Err(String::from("wrong error kind"));
+        }
+    } else {
+        return Err(String::from("should have been error"));
+    }
 
     Ok(())
 }
--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -6,8 +6,8 @@ use gauss_kronrod_integration::Integrand;
 
 /* f1(x) = x^alpha * log(1/x) */
 /* integ(f1,x,0,1) = 1/(alpha + 1)^2 */
-pub struct Function1 {
-    pub alpha: f64,
+pub(crate) struct Function1 {
+    pub(crate) alpha: f64,
 }
 
 impl Integrand for Function1 {
@@ -19,8 +19,8 @@ impl Integrand for Function1 {
 
 /* f2(x) = 4^-alpha / ((x-pi/4)^2 + 16^-alpha) */
 /* integ(f2,x,0,1) = arctan((4-pi)4^(alpha-1)) + arctan(pi 4^(alpha-1)) */
-pub struct Function2 {
-    pub alpha: f64,
+pub(crate) struct Function2 {
+    pub(crate) alpha: f64,
 }
 
 impl Integrand for Function2 {
@@ -32,8 +32,8 @@ impl Integrand for Function2 {
 
 /* f3(x) = cos(2^alpha * sin(x)) */
 /* integ(f3,x,0,pi) = pi J_0(2^alpha) */
-pub struct Function3 {
-    pub alpha: f64,
+pub(crate) struct Function3 {
+    pub(crate) alpha: f64,
 }
 
 impl Integrand for Function3 {
@@ -43,7 +43,25 @@ impl Integrand for Function3 {
     }
 }
 
-pub fn test_relative_error(
+/* f16(x) = x^(alpha - 1) / (1 + 10 x)^2*/
+pub(crate) struct Function16 {
+    pub(crate) alpha: i32,
+}
+
+impl Integrand for Function16 {
+    fn evaluate(&self, x: &f64) -> f64 {
+        let alpha = self.alpha;
+        if x.to_bits() == 0f64.to_bits() && alpha == 1 {
+            1.0
+        } else if x.to_bits() == 0f64.to_bits() && alpha > 1 {
+            0.0
+        } else {
+            x.powi(alpha - 1) / (1.0 + 10.0 * x).powi(2)
+        }
+    }
+}
+
+pub(crate) fn test_relative_error(
     calculated: f64,
     target: f64,
     relative_error: f64,
@@ -80,7 +98,7 @@ pub fn test_relative_error(
     Ok(())
 }
 
-pub fn test_absolute_error(
+pub(crate) fn test_absolute_error(
     calculated: f64,
     target: f64,
     absolute_error: f64,
@@ -110,7 +128,7 @@ pub fn test_absolute_error(
     Ok(())
 }
 
-pub fn test_int(
+pub(crate) fn test_int(
     calculated: usize,
     target: usize,
     description: &str,
