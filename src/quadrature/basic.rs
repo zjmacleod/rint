cd9c597ca227bef8f0dac6bd5c6c0d8af089bad1
Zander Macleod <zmacleod@pm.me>
2026-03-11 14:59:54 +0100

    Change quadrature/basic.rs docs

--- src/quadrature/basic.rs
+++ src/quadrature/basic.rs
@@ -5,23 +5,47 @@ use crate::Limits;
 
 /// A non-adaptive Gauss-Kronrod integrator.
 ///
-/// This is the most basic integration routine provided by this library. It applies an n-point
-/// Gauss-Kronrod integration [`Rule`] to a user supplied function, which is a struct implementing
-/// the [`Integrand`] trait.
-/// The integration routine tries to be as efficient as possible by reusing function
-/// evaluations of the lower `n`-point Gauss integration in the calculation of the higher-order
-/// Kronrod extension. The total number of function evaluations is `n`.
-/// The available Gauss-Kronrod integration rules are:
-/// * 15-point: 7-point Gauss, 15-point Kronrod ([`Rule::gk15()`])
-/// * 21-point: 10-point Gauss, 21-point Kronrod ([`Rule::gk21()`])
-/// * 31-point: 15-point Gauss, 31-point Kronrod ([`Rule::gk31()`])
-/// * 41-point: 20-point Gauss, 41-point Kronrod ([`Rule::gk41()`])
-/// * 51-point: 25-point Gauss, 51-point Kronrod ([`Rule::gk51()`])
-/// * 61-point: 30-point Gauss, 61-point Kronrod ([`Rule::gk61()`])
+/// The [`Basic`] integrator is a non-adaptive integrator designed for approximating
+/// one-dimensional integrals of the form,
+/// $$
+/// I = \int_{b}^{a} f(x) dx
+/// $$
+/// using Gauss-Kronrod integration rules. The function $f(x)$ is encoded in [`Basic`] as something
+/// implementing the [`Integrand`] trait. A Gaussian numerical integration rule approximates an
+/// integral of a function by performing a weighted sum of the function evaluated at defined
+/// points/abscissae. The order of an integration rule, $n$, denotes the number of abscissae,
+/// $x_{i}$, at which the function is evaluated and the number of weights $w_{i}$ for the weighted
+/// sum, such that the approximation is,
+/// $$
+/// I \approx \sum_{i = 1}^{n} W_{i} f(X_{i}) = I_{n}
+/// $$
+/// where the $X_{i}$ and $W_{i}$ are the rescaled abscissae and weights,
+/// $$
+/// X_{i} = \frac{b + a + (a - b) x_{i}}{2} ~~~~~~~~ W_{i} = \frac{(a - b) w_{i}}{2}
+/// $$
+/// where the `limits` $a$ and $b$ are encoded in [`Basic`] via [`Limits`] passed to the
+/// constructor. A Gauss-Kronrod integration rule combines two rules of different order for
+/// efficient estimation of the numerical error. The rules for an $n$-point Gauss-Kronrod rule
+/// contain $m = (n - 1) / 2$ abscissae _shared_ by the Gaussian and Kronrod rules and an extended
+/// set of $n - m$ Kronrod abscissae. The weighted sum of the full set of $n$ Kronrod function
+/// evaluations are used to approximate the result of the integration, while the weighted sum of
+/// the lower order set of $m$ Gaussian points are used to calculate the numerical error in the
+/// routine,
+/// $$
+/// E = |I_{n} - I_{m}|
+/// $$
+/// This approach is efficient, as only $n$ total function evaluations are required to obtain the
+/// result approximation and error estimate. See [`Rule`] for the available Gauss-Kronrod rules.
 ///
-/// The user defines an [`Integrand`] to be integrated using a fixed Gauss-Kronrod
-/// integration [`Rule`] between the two integration limits `lower` and `upper`.
-/// By convention `upper > lower`, however this is not mandatory.
+/// This routine is *not* adaptive, and runs exactly once on the input function. Thus, it is only
+/// suitable for the integration of smooth functions with no problematic regions in the integration
+/// region. If higher accuracy is required then the [`Adaptive`] or [`AdaptiveSingularity`]
+/// integrators should be used instead.
+///
+/// [`Adaptive`]: crate::quadrature::Adaptive
+/// [`AdaptiveSingularity`]: crate::quadrature::AdaptiveSingularity
+///
+/// # Example
 ///
 ///```rust
 /// use rint::{Limits, Integrand};
@@ -77,10 +101,9 @@ where
 {
     /// Create a new [`Basic`] integrator.
     ///
-    /// The user first defines a `function` which is a `struct` implementing the
-    /// [`Integrand`] trait and selects a Gauss-Kronrod quadrature [`Rule`],
-    /// `rule`, to integrate the function with between the integration [`Limits`],
-    /// `limits`.
+    /// The user first defines a `function` which is a `struct` implementing the [`Integrand`]
+    /// trait and selects a Gauss-Kronrod quadrature [`Rule`], `rule`, to integrate the function
+    /// with between the integration [`Limits`], `limits`.
     pub const fn new(function: &'a I, rule: &'a Rule, limits: Limits) -> Self {
         Self {
             function,
@@ -93,9 +116,9 @@ where
     ///
     /// Applies the user supplied integration rule to obtain an [`IntegralEstimate`], which is the
     /// numerically evaluated estimate of the integral value and error, as well as the number of
-    /// function evaluations and integration routine iterations.
-    /// Note: for the [`Basic`] integrator the number of iterations is `1`, and the number of
-    /// function evaluations for an `n`-point integration rule is `n`.
+    /// function evaluations and integration routine iterations. Note: for the [`Basic`] integrator
+    /// the number of iterations is 1, and the number of function evaluations for an $n$-point
+    /// integration rule is $n$.
     #[must_use]
     pub fn integrate(&self) -> IntegralEstimate<I::Scalar> {
         let integral = self.integrator().integrate();
@@ -112,6 +135,7 @@ where
         self.limits
     }
 
+    /// Create an [`Integrator`] from the integration data stored in a [`Basic`].
     #[must_use]
     const fn integrator(&self) -> Integrator<'_, I> {
         Integrator::new(self.function, self.rule, self.limits)
