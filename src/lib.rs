930181346851f29d777874a4eee877d5ef268e6b
Zander Macleod <zmacleod@pm.me>
2026-04-01 13:07:31 +0200

    Add Send/Sync tests for Error types

--- src/lib.rs
+++ src/lib.rs
@@ -790,3 +790,28 @@ impl<T: ScalarF64> fmt::Display for IntegrationError<T> {
 }
 
 impl<T: ScalarF64> error::Error for IntegrationError<T> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_error_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<Error<f64>>();
+        assert_send::<Error<Complex<f64>>>();
+        assert_send::<IntegrationError<f64>>();
+        assert_send::<IntegrationError<Complex<f64>>>();
+        assert_send::<InitialisationError>();
+    }
+
+    #[test]
+    fn test_error_sync() {
+        fn assert_sync<T: Sync>() {}
+        assert_sync::<Error<f64>>();
+        assert_sync::<Error<Complex<f64>>>();
+        assert_sync::<IntegrationError<f64>>();
+        assert_sync::<IntegrationError<Complex<f64>>>();
+        assert_sync::<InitialisationError>();
+    }
+}
